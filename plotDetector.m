474a1d4999b07e74b345b07cf5a058445c88ba0b
Faaiz <syedfaaiz2014@gmail.com>
2017-06-01 06:18:44 +0500

    first commit

---
+++ plotDetector.m
@@ -0,0 +1,68 @@
+%% load data into matlab
+hold on
+clear all
+load('cellarray_ways.mat', 'cellarray_ways');
+
+%Ways = zeros(length(cellarray_ways),length(cellarray_ways{cell_num}),2);
+%% find block
+for cell_num = 1:length(cellarray_ways)
+    for point_num = 1:length(cellarray_ways{cell_num})
+        Ways(cell_num,point_num,1) = cellarray_ways{cell_num}{point_num}(1);
+        Ways(cell_num,point_num,2) = cellarray_ways{cell_num}{point_num}(2);
+    end
+end
+
+
+%% load data into matlab
+
+load('cellarray_ways.mat', 'cellarray_ways');
+
+%Ways = zeros(length(cellarray_ways),length(cellarray_ways{cell_num}),2);
+%% find block
+for cell_num = 1:length(cellarray_ways)
+    for point_num = 1:length(cellarray_ways{cell_num})
+        Ways(cell_num,point_num,1) = cellarray_ways{cell_num}{point_num}(1);
+        Ways(cell_num,point_num,2) = cellarray_ways{cell_num}{point_num}(2);
+    end
+end
+
+
+
+hold on
+X={};
+Y={};
+
+
+
+    
+for cell_num = 1:length(cellarray_ways)
+    %plot(Ways(cell_num,:,1) , Ways(cell_num,:,2));
+  
+ manual_ways_toshow=[1:319]
+%% manual_ways_toshow=[63,80,74,97];
+    if (sum(cell_num==manual_ways_toshow)==1)
+        
+        disp([num2str(cell_num) ,'_']);
+        %following code is used to detect if the path(set of points) is a
+        %relatively straight line
+        [p,struct] = polyfit( Ways(cell_num,1:length(cellarray_ways{cell_num}),1) , Ways(cell_num,1:length(cellarray_ways{cell_num}),2) ,1)
+        if(struct.df<8 )% .df is a measure of straightness (dont know/need the exact details)
+            
+            plot( Ways(cell_num,1:length(cellarray_ways{cell_num}),1) , Ways(cell_num,1:length(cellarray_ways{cell_num}),2) ,'g');
+            X{end+1}= Ways(cell_num,1:length(cellarray_ways{cell_num}),1);
+            Y{end+1}= Ways(cell_num,1:length(cellarray_ways{cell_num}),2);
+        else
+            %dont plot lines that arent straight
+            disp('is not a st line')
+        end
+        % number the lines for easy marking of paths
+        text(max(Ways(cell_num,1:length(cellarray_ways{cell_num}),1)),max(Ways(cell_num,1:length(cellarray_ways{cell_num}),2)),num2str(cell_num))
+        disp('^');
+    end
+end
+
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
