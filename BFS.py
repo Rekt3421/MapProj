b36584ba245733a294d5188449d162341fa1a266
Faaiz <syedfaaiz2014@gmail.com>
2017-06-20 20:24:02 +0500

    plots found atlast

--- BFS.py
+++ BFS.py
@@ -8,22 +8,22 @@ def arrange(items):
 		sorted(items)
 	return items	
 
-def BFS(start , end , data, path,way,count):
+def BFS(start , end , data, path,way,count,offset):
 	if count >100000:
 		return []
 	neighbours = [] 
 	biggerways = []
 	temp = 0
 	for each in data:
-		if (each[2] == start or each[3] == start )  and each[5] not in way  :
-			if each[2] == start and each[3] : 
+		if (each[2] == start or each[3] == start )  
+			if each[2] == start  : 
 				neighbours.append(each[3])
 				biggerways.append(each[5])
 				
 				cnt = 0
 				tmp = [0,1]
 				
-			elif each[3] == start and each[2]  and each[4] not in way: 
+			elif each[3] == start : 
 				neighbours.append(each[2])
 				biggerways.append(each[4])
 				
@@ -39,7 +39,8 @@ def BFS(start , end , data, path,way,count):
 		
 		return way
 			
-
+	if end in path or end in way:
+			return way		
 
 	cnt = 0
 	for each in neighbours:
@@ -49,9 +50,9 @@ def BFS(start , end , data, path,way,count):
 		ind = neighbours.index(each)
 		way1.append(biggerways[ind])
 	
-		return BFS(each,end,data,path1,way1,count+1)	
+		return BFS(each,end,data,path1,way1,count+1,offset)	
 		cnt = cnt +1
-
+	return way		
 plots = []
 
 intersections = sio.loadmat('intersections.mat')
@@ -65,20 +66,40 @@ tmp[1] = 2
 plot_num = 0
 res = [0 ,0 ,0,0,0,0]
 plots = []
-for each in intersections:
-	if not each[4] == each[5]: 
-		
-		data = intersections
+print len(intersections)
+for each in intersections: 
+	data = intersections
 		
 
 
-		res = (BFS(each[3],each[2],data,[each[3]],[each[5]],1))
-		if not res == None:
-			res = sorted(res)
-			if res not in plots :
-				plots.append(res)
-				plot_num = plot_num + 1
-				print res
+	res = (BFS(each[3],each[2],data,[each[3]],[each[5]],1,0))
+	if not res == None:
+		res = sorted(res)
+		if res not in plots and len(res) > 3:
+			plots.append(res)
+			plot_num = plot_num + 1
+			print res
+	res = (BFS(each[2],each[3],data,[each[2]],[each[4]],1,0))
+	if not res == None:
+		res = sorted(res)
+		if res not in plots and len(res) > 3:
+			plots.append(res)
+			plot_num = plot_num + 1
+			print res
+	res = (BFS(each[2],each[3],data,[each[2]],[each[4]],1,1))
+	if not res == None:
+		res = sorted(res)
+		if res not in plots and len(res) > 3:
+			plots.append(res)
+			plot_num = plot_num + 1
+			print res
+	res = (BFS(each[3],each[2],data,[each[3]],[each[5]],1,1))
+	if not res == None:
+		res = sorted(res)
+		if res not in plots and len(res) > 3:
+			plots.append(res)
+			plot_num = plot_num + 1
+			print res						
 	count = count +1 
 
-sio.savemat('C:\Users\Safi syed\Desktop\map project graphics\plots',mdict={'arr': plots},appendmat=True)
+sio.savemat('C:\Users\Safi syed\Desktop\map project graphics\ blocks',mdict={'arr': plots},appendmat=True, format = '5')
